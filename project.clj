19235391062b25a7fe83612c0205a2623fdd6f6a
Mike Sperber <sperber@deinprogramm.de>
2014-06-03 10:46:01 +0200

    Next dev cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reacl "0.2.0"
+(defproject reacl "0.3.0-SNAPSHOT"
   :description "ClojureScript wrappers for programming with React"
   :url "http://github.com/active-group/reacl"
   :license {:name "Eclipse Public License"

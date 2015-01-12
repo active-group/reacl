8cb604d2db791c4ea57c11a9e1765a986f5441e8
David Frese <david.frese@active-group.de>
2015-01-12 15:07:53 +0100

    Prepare this branch as the version 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reacl "0.7.0"
+(defproject reacl "1.0-SNAPSHOT"
   :description "ClojureScript wrappers for programming with React"
   :url "http://github.com/active-group/reacl"
   :license {:name "Eclipse Public License"

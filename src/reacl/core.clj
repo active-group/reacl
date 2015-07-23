c7896ebed3a8d4412ef4f7ea606a4c7359191915
David Frese <david.frese@active-group.de>
2015-07-23 11:47:38 +0200

    Add and fix some doc strings.

--- src/reacl/core.clj
+++ src/reacl/core.clj
@@ -100,13 +100,13 @@
   will do a (=) comparison with the current values. Implement this, if
   you want to prevent an update on every app-state change for example.
 
-  (component-will-update next-app-state next-local-state next-arg1 next-arg2 ...)
+  `(component-will-update next-app-state next-local-state next-arg1 next-arg2 ...)`
   Called immediately before an update.
   
-  (component-did-update prev-app-state prev-local-state prev-arg1 prev-arg2 ...)
+  `(component-did-update prev-app-state prev-local-state prev-arg1 prev-arg2 ...)`
   Called immediately after an update. The component can update it's DOM here.
   
-  (component-will-unmount)
+  `(component-will-unmount)`
   The component can cleanup it's DOM here for example.
 
   Example:
@@ -255,7 +255,7 @@
 
   The syntax is
 
-      (reacl.core/defclass <name> <app-state> [<param> ...]
+      (reacl.core/defclass <name> [<this-name> [<app-state-name> [<local-state-name>]]] [<param> ...]
         render <renderer-exp>
         [initial-state <initial-state-exp>]
         [<lifecycle-method-name> <lifecycle-method-exp> ...]
@@ -266,7 +266,7 @@
   This expands to this:
 
       (def <name>
-        (reacl.core/class <name> <app-state> [<param> ...]
+        (reacl.core/class <name> [<this-name> [<app-state-name> [<local-state-name>]]] [<param> ...]
           render <renderer-exp>
           [initial-state <initial-state-exp>]
           [<lifecycle-method-name> <lifecycle-method-exp> ...]
@@ -277,6 +277,61 @@
   `(def ~?name (reacl.core/class ~(str ?name) ~@?stuff)))
 
 (defmacro view
+  "Create a Reacl view, which is like a class, but exposes no state (applications state).
+   Views can be instantiated in the same way as virtual dom elements.
+
+  The syntax is
+
+      (reacl.core/view <name> [<this-name> [<local-state-name>]] [<param> ...]
+        render <renderer-exp>
+        [initial-state <initial-state-exp>]
+        [local [<local-name> <local-expr>]]
+        [handle-message <messager-handler-exp>]
+        [<lifecycle-method-name> <lifecycle-method-exp> ...])
+
+  `<name>` is a name for the view, for debugging purposes.
+
+  A number of names are bound in the various expressions in the body
+  of reacl.core/class:
+
+  - `<this-name>` is bound to the component object itself
+  - `<local-state-name>` is bound to the component-local state
+  - the `<param>` ... names are the explicit arguments of instantiations
+
+  The `local` and `<renderer-exp>` clauses are identical
+  to those of the [[class]] macro.
+
+  The `handle-message` function accepts a message sent to the
+  component via [[reacl.core/send-message!]].  It's expected to return
+  a value specifying a new component-local state,
+  via [[reacl.core/return]]. Specifying an `:app-state` clause
+  in [[reacl.core/return]] is not allowed.
+
+  A view can be invoked to yield a component as a function as follows:
+
+  `(<view> <arg> ...)`
+
+  A lifecycle method can be one of:
+
+    `component-will-mount` `component-did-mount`
+    `component-will-receive-args` `should-component-update?`
+    `component-will-update` `component-did-update` `component-will-unmount`
+
+  The methods `component-did-mount` and `component-will-unmount` are
+  the same as for classes. The methods `component-will-mount` and
+  `component-will-receive-args` have the same arguments as for
+  classes, but only the local component state may be updated by them.
+
+  The methods `should-component-update?`, `component-did-update` and
+  `component-will-unmount` are similar to those of [[class]], without
+  the application state arguments. Specifically:
+
+  - `(should-component-update? next-local-state next-arg1 next-arg2 ...)`
+
+  - `(component-did-update prev-local-state prev-arg1 prev-arg2 ...)`
+
+  - `(component-will-update next-local-state next-arg1 next-arg2 ...)`
+  "
   [?name & ?stuff]
   (let [[?component ?stuff] (split-symbol ?stuff `component#)
         ?app-state `app-state#
@@ -303,6 +358,28 @@
                         ~@?clauses))))
 
 (defmacro defview
+  "Define a Reacl view, see [[view]] for documentation.
+
+  The syntax is
+
+      (reacl.core/defview <name> [<this-name> [<local-state-name>]] [<param> ...]
+        render <renderer-exp>
+        [initial-state <initial-state-exp>]
+        [<lifecycle-method-name> <lifecycle-method-exp> ...]
+        [handle-message <messager-handler-exp>]
+
+        <event-handler-name> <event-handler-exp> ...)
+
+  This expands to this:
+
+      (def <name>
+        (reacl.core/view <name> [<this-name> [<local-state-name>]] [<param> ...]
+          render <renderer-exp>
+          [initial-state <initial-state-exp>]
+          [<lifecycle-method-name> <lifecycle-method-exp> ...]
+          [handle-message <messager-handler-exp>]
+
+          <event-handler-name> <event-handler-exp> ...))"
   [?name & ?stuff]
   `(def ~?name (reacl.core/view ~(str ?name) ~@?stuff)))
 

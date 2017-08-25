167e130fe4a5ecbe5d1c31217cfe8f7f97d767fb
Mike Sperber <sperber@deinprogramm.de>
2017-08-25 13:40:27 +0200

    Unbreak variadic case for IFn.

--- src/reacl2/core.cljs
+++ src/reacl2/core.cljs
@@ -863,7 +863,7 @@
           (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18]
             (instantiate-embedded-internal-v1 this app-state reaction [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18]))
           (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 rest]
-            (apply instantiate-embedded-internal-v1 this app-state reaction (concat [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18] rest)))
+            (instantiate-embedded-internal-v1 this app-state reaction (concat [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18] rest)))
           IReaclClass
           (-instantiate-toplevel-internal [this rst]
             (instantiate-toplevel-internal this has-app-state? rst))

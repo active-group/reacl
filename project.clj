f7970adfaa3255788d00e3670ebfafa37731cc29
Mike Sperber <sperber@deinprogramm.de>
2019-03-07 09:47:03 +0100

    2.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reacl "2.1.0-SNAPSHOT"
+(defproject reacl "2.1.0"
   :description "ClojureScript wrappers for programming with React"
   :url "http://github.com/active-group/reacl"
   :license {:name "Eclipse Public License"

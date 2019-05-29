c1be92c4c03bbf91dee5f7ed9caeb29384b22245
David Frese <david.frese@active-group.de>
2019-05-29 13:55:16 +0200

    Fix docs some more.

--- src/reacl2/test_util/beta.cljs
+++ src/reacl2/test_util/beta.cljs
@@ -170,7 +170,8 @@
   (fn [class & args]
     (second (apply mount* class args))))
 
-(def ^{:arglists '(c f & args)
+(def ^{:arglists '([comp f & args]
+                   [env f & args])
        :doc "Calls `(f comp & args)` if called with a component, or if
   called with a test environment, with the currently mounted toplevel
   class instance. In any case, after `f` has been evaluated for its
@@ -205,7 +206,7 @@
       (assert (reacl/reacl-class? (reacl/component-class inst)))
       (apply f inst args))))
 
-(def ^{:arglists '(c)
+(def ^{:arglists '([comp] [env])
        :doc "Return the current local-state of a component, or if
   given a test environment, of the toplevel component. Throws if it is
   not mounted."}
@@ -213,7 +214,7 @@
   (fn [c]
     (reacl/extract-local-state (get-reacl-instance c))))
 
-(def ^{:arglists '(c msg)
+(def ^{:arglists '([env msg] [comp msg])
        :doc "Sends the given message to the given component, or the component
   currently mounted in the given test environment. Returns a changed
   app-state and actions from the toplevel class, in the form of a

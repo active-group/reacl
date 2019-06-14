a11fd02726abb508f6dca735206d37fb5e4eba81
David Frese <david.frese@active-group.de>
2019-06-14 11:26:36 +0200

    This branch is planned to be version 2.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reacl "2.1.2-SNAPSHOT"
+(defproject reacl "2.2.0-SNAPSHOT"
   :description "ClojureScript wrappers for programming with React"
   :url "http://github.com/active-group/reacl"
   :license {:name "Eclipse Public License"

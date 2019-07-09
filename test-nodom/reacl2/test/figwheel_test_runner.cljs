20f451b0a96238b3d9577715d2948fdd56bef55c
Simon Härer <simon.haerer@active-group.de>
2019-07-09 15:30:04 +0200

    Unbreak travis tests

--- test-nodom/reacl2/test/figwheel_test_runner.cljs
+++ test-nodom/reacl2/test/figwheel_test_runner.cljs
@@ -1,6 +1,6 @@
 (ns reacl2.test.figwheel-test-runner
   (:require [figwheel.main.testing :refer-macros [run-tests-async]]
-            [reacl2.test.core]))
+            [reacl2.test.core-test]))
 
 
 (defn -main [& args]

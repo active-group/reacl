d1588bfc30f374112ce081b167479fbe12f1d2a1
David Frese <david.frese@active-group.de>
2020-01-23 13:04:11 +0100

    New dev cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reacl "2.2.0"
+(defproject reacl "2.2.1-SNAPSHOT"
   :description "ClojureScript wrappers for programming with React"
   :url "http://github.com/active-group/reacl"
   :license {:name "Eclipse Public License"

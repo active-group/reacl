4186711353f6ac54f30ec955d2d54031a714d6c5
David Frese <david.frese@active-group.de>
2020-07-02 16:12:51 +0200

    Fix higher arities of reacl-1 classes, which broke in 2.2.0.

--- src/reacl2/core.cljs
+++ src/reacl2/core.cljs
@@ -1579,39 +1579,39 @@ component (like the result of an Ajax request).
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals []))
           (-invoke [this app-state reaction a1]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1]))
-          (-invoke [this app-state reaction compute-locals a1 a2]
+          (-invoke [this app-state reaction a1 a2]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3]
+          (-invoke [this app-state reaction a1 a2 a3]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4]
+          (-invoke [this app-state reaction a1 a2 a3 a4]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17]))
-          (-invoke [this app-state reaction compute-locals a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 rest]
+          (-invoke [this app-state reaction a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 rest]
             (instantiate-embedded-internal-v1 this app-state reaction compute-locals (concat [a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17] rest)))
           IReaclClass
           (-instantiate-embedded-internal [this rst]

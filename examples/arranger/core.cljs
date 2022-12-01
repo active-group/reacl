e517c19fe5e6e20667afbf0bf5fc81386f3c6f33
Marcus Crestani <marcus.crestani@active-group.de>
2022-12-01 11:52:52 +0100

    More on arranger example.

--- examples/arranger/core.cljs
+++ examples/arranger/core.cljs
@@ -11,28 +11,61 @@
   select?
   [selection select-selection])
 
+(define-record-type Unselect
+  unselect
+  unselect?
+  [selection unselect-selection])
+
+(defn remove-selection
+  [selected selection]
+  (remove #(= selection %) selected))
+
+(defn add-selection
+  [selected selection]
+  (concat selected [selection]))
+
+(defn selection-
+  [s1 s2]
+  (vec (reduce (fn [result selection]
+                 (remove-selection result selection))
+               s1
+               s2)))
+
 (reacl/defclass arranger this arranged
   [available render-fn]
 
-  ;local-state []
-
   render
-  (dom/div
-    (apply dom/select
-           {:onchange #(reacl/send-message! this
-                                            (select (-> % .-target .-value)))}
-           (dom/option {:value ::not-selected} "Select...")
-           (mapv (fn [a] (dom/option {:value a} a))
-                 available))
+  (letfn [(render-selection
+            [thing]
+            (dom/div
+              (dom/button {:onclick #(reacl/send-message! this
+                                                          (unselect thing))}
+                          "X")
+              (dom/div (render-fn thing))))]
+    (dom/div
+      (boot-strap-menu-bar
+      (apply dragabble
 
-    (apply dom/div
-           (mapv render-fn arranged)))
+             dom/li
+             (mapv #(render-selection %) arranged))
+
+      (let [selectable (selection- available arranged)]
+        (when-not (empty? selectable)
+          (apply dom/select
+                 {:value ::not-selected
+                  :onchange #(reacl/send-message! this
+                                                  (select (-> % .-target .-value)))}
+                 (dom/option {:value ::not-selected} "Select...")
+                 (mapv (fn [a] (dom/option {:value a} a)) selectable))))))
 
   handle-message
   (fn [msg]
     (cond
       (select? msg)
-      (reacl/return :app-state (concat arranged [(select-selection msg)])))))
+      (reacl/return :app-state (add-selection arranged (select-selection msg)))
+
+      (unselect? msg)
+      (reacl/return :app-state (remove-selection arranged (unselect-selection msg))))))
 
 (reacl/defclass root this state []
   render

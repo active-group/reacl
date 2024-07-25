b98dd08c6600d076f13fa86cab7fc4edc200d2b3
Tobias Paulus <tobias.paulus@active-group.de>
2024-07-25 14:57:43 +0200

    Add de.active-group to defproject name.
    This also serves as a maven group-id.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reacl "2.2.10"
+(defproject de.active-group/reacl "2.2.10"
   :description "ClojureScript wrappers for programming with React"
   :url "http://github.com/active-group/reacl"
   :license {:name "Eclipse Public License"
